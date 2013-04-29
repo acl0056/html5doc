6d642a06b96010e395124420383683eed4ce8d1d
Adam Lockhart <adamlockhart@me.com>
2013-04-29 13:51:43 -0500

    Update html5doc.js
    Added MIT license text.

--- html5doc.js
+++ html5doc.js
@@ -1,4 +1,13 @@
 /*	File: html5doc.js
+
+The MIT License (MIT)
+Copyright (c) 2013 Adam Lockhart
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 /*	Class: Html5Doc
 	Html5Doc lets you store and retrieve document objects from local storage.  All work is done in class methods, because an instance IS the document that is saved to local storage.  Any properties you give to the document must be serializable into a JSON string.  Html5Doc uses a document store name, which allows you to easily group your documents by type, user, or whatever your implementation requires.
